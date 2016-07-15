97a07dd67777a4f9f1946863c5b7765522c42799
Matt Chapman <matt@matthewchapman.co.uk>
2016-07-15 17:12:25 +0100

    Initial commit

---
+++ Bug.py
@@ -0,0 +1,77 @@
+import Mappings
+
+
+# contains bug metadata
+class Bug:
+    def __init__(self):
+        self.id = 0
+        self.name = 'default_name'
+        self.project = 'default_project'
+        self.component = 'default_component'
+        self.status = 'default_status'
+        self.resolution = 'default_resolution'
+        self.priority = 0
+        self.severity = 'default_severity'
+        self.assignee = 'default_assignee'
+        self.fogStatus = 0  # fogbugz specific status index
+        self.comments = []
+
+    # map all metadata etc
+    def fixbug(self):
+        self.assignee = Mappings.users.get(self.assignee, self.assignee)
+        self.project = Mappings.projects.get(self.project, self.project)
+
+        if self.status == 'RESOLVED':
+            self.fogStatus = Mappings.resolutions.get(self.resolution, self.fogStatus)
+        else:
+            self.fogStatus = Mappings.statuses.get(self.status, self.fogStatus)
+
+        if self.priority is not None:
+            self.priority = Mappings.priorities.get(self.priority, self.priority)
+
+    # utility method to print out bug details
+    def printdetails(self):
+        if self.project is not None:
+            print('PROJECT: ' + self.project)
+
+        print('BUG ID: ' + self.id)
+
+        if self.name is not None:
+            print('TITLE: ' + self.name)
+        if self.component is not None:
+            print('COMPONENT: ' + self.component)
+        if self.status is not None:
+            print('STATUS: ' + self.status)
+        if self.resolution is not None:
+            print('RESOLUTION: ' + self.resolution)
+        if self.priority is not None:
+            print('PRIORITY: ' + str(self.priority))
+        if self.assignee is not None:
+            print('ASSIGNEE: ' + self.assignee)
+        if self.fogStatus is not None:
+            print('FOGBUGZ STATUS: ' + str(self.fogStatus) + '\n')
+
+        for comment in self.comments:
+            if len(self.comments) > 0:
+                if comment.who is not None and comment.when is not None:
+                    print(comment.who + ' said on ' + comment.when + ':')
+                if comment.text is not None:
+                    print(comment.text + '\n\n')
+
+        print('====================\n')
+
+
+# contains comments, in the comment structure in the bug
+class Comment:
+    def __init__(self):
+        self.who = 'default_name'
+        self.when = 'default_time'
+        self.text = 'default_text'
+
+
+# structure to store all processed bugs
+class BugList:
+    def __init__(self):
+        self.bugs = set()
+        self.projects = set()
+        self.users = set()
---
+++ BugParser.py
@@ -0,0 +1,113 @@
+import xml.etree.ElementTree as ET
+
+import fogbugz
+
+import FbSettings
+from Bug import Bug, Comment, BugList
+
+# should we submit to the DB?
+doSubmission = True
+printDebug = True
+
+parser = ET.XMLParser()
+# deal correctly with bad elements
+parser.parser.UseForeignDTD(True)
+
+# parse the structure
+tree = ET.parse(FbSettings.PATH)
+root = tree.getroot()
+
+bugList = BugList()
+
+# get all of the bugs
+for currentbug in root.iter('bug'):
+
+    # deal with all the bug metadata
+    bugObject = Bug()
+    bugObject.id = currentbug.find('bug_id').text
+    bugObject.name = currentbug.find('short_desc').text
+    bugObject.project = currentbug.find('product').text
+    bugObject.component = currentbug.find('component').text
+    bugObject.status = currentbug.find('bug_status').text
+    bugObject.resolution = currentbug.find('resolution').text
+    bugObject.priority = currentbug.find('priority').text
+    bugObject.severity = currentbug.find('bug_severity').text
+    bugObject.assignee = currentbug.find('assigned_to').get('name')
+
+    # deal with all of the comments
+    for comment in currentbug.iter('long_desc'):
+
+        # get comment info
+        commentObject = Comment()
+        commentObject.who = comment.find('who').attrib.get('name')
+        commentObject.text = comment.find('thetext').text
+        commentObject.when = comment.find('bug_when').text
+
+        if commentObject.text is None:
+            commentObject.text = 'No comment body. Check bugzilla for attachments'
+
+        # apppend comment to current bug's comment list
+        bugObject.comments.append(commentObject)
+
+    # now we adjust the metadata to match accounts in FogBugz...
+    bugObject.fixbug()
+
+    # print the resulting bug, if we want to
+    if printDebug:
+        bugObject.printdetails()
+
+    # add unique objects to their sets
+    bugList.bugs.add(bugObject)
+    bugList.projects.add(bugObject.project)
+    bugList.users.add(bugObject.assignee)
+
+# only do bug submission to the DB if it is enabled
+if doSubmission:
+    iterator = 1
+
+    for bug in bugList.bugs:
+
+        # print progress
+        print 'Processing Bug ' + str(iterator) + ' of ' + str(len(bugList.bugs)) + '...'
+
+        iterator += 1
+        comments = ''
+
+        # log into the API
+        fb = fogbugz.FogBugz(FbSettings.URL, FbSettings.TOKEN)
+
+        # concat all the comments together
+        if len(bug.comments) > 0:
+            for comment in bug.comments:
+                comments += comment.who + ' said on ' + comment.when + ':\n----------\n' + comment.text + '\n\n'
+        else:
+            comments = 'No comments.'
+
+        # submit the bug
+        response = fb.new(
+            sProject=bug.project,
+            sTitle=bug.name,
+            sEvent='Imported from Bugzilla. Original Bug ID: ' + bug.id + '\n\nOriginal comments: \n\n'
+                                                                          '====================\n\n' + comments,
+            sPersonAssignedTo=bug.assignee,
+            ixStatus=bug.fogStatus,
+            ixPriority=bug.priority
+        )
+
+        # if the current bug is resolved, attempt to resolve and close the last submitted bug
+        if bug.status == 'RESOLVED':
+            try:
+                fb.resolve(ixBug=response.case['ixbug'], ixStatus=bug.fogStatus)
+                fb.close(ixBug=response.case['ixbug'])
+            except fogbugz.FogBugzAPIError:
+                print 'API error'
+
+# print out some general info on completion
+print 'Processed ' + str(len(bugList.bugs)) + ' bugs...\n'
+print 'Covering ' + str(len(bugList.projects)) + ' projects:'
+for project in bugList.projects:
+    print project
+print '\n'
+print 'Assigned to ' + str(len(bugList.users)) + ' users:'
+for user in bugList.users:
+    print user
